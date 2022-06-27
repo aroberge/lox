6db93b1d4fa460039be0eccba1d6c776e60d828b
André Roberge <andre.roberge@gmail.com>
2022-06-26 23:04:54 -0300

    Simplifying Lox class, and using __main__ properly.

--- pylox/__main__.py
+++ pylox/__main__.py
@@ -2,4 +2,21 @@ import sys
 
 from pylox.lox import Lox
 
-Lox(sys.argv)
+
+def main() -> None:
+    args = sys.argv
+    if len(args) > 2:
+        print("Usage: py -m lox [script]")
+        sys.exit(64)
+    lox = Lox()
+    if len(args) == 2:
+        lox.run_file(args[1])
+    else:
+        try:
+            lox.run_prompt()
+        except KeyboardInterrupt:
+            print("\n   Goodbye!")
+            sys.exit(64)
+
+
+main()
--- pylox/lox.py
+++ pylox/lox.py
@@ -16,23 +16,9 @@ from .lexer.scanner import Scanner
 class Lox:
     """The class used as a starting point"""
 
-    had_error = False
-
-    def __init__(self, args: list[str]) -> None:
+    def __init__(self) -> None:
         """Entrypoint for the program"""
-        # use >> for prompt to distinguish from my terminal prompt
-        self.prompt = ">> "
-        if len(args) > 2:
-            print("Usage: py -m lox [script]")
-            sys.exit(64)
-        elif len(args) == 2:
-            self.run_file(args[1])
-        else:
-            try:
-                self.run_prompt()
-            except KeyboardInterrupt:
-                print("\n   Goodbye!")
-                sys.exit(64)
+        self.had_error: bool = False
 
     def run_file(self, filename: str) -> None:
         """Runs code from a file"""
@@ -45,14 +31,16 @@ class Lox:
     def run_prompt(self) -> None:
         """Simple REPL"""
         print("pylox REPL\n")
+        # use >> for prompt to distinguish from my terminal prompt
+        prompt = ">> "
         while True:
-            line = input(self.prompt)
+            line = input(prompt)
             if not line:
                 break
             try:
                 self.run(line)
             except LoxSyntaxError as exc:
-                print(" " * (exc.column + len(self.prompt)) + "^-- LoxSyntaxError.")
+                print(" " * (exc.column + len(prompt)) + "^-- LoxSyntaxError.")
                 print(f"{exc.message}\n")
                 self.had_error = True
             else:
@@ -64,18 +52,3 @@ class Lox:
         tokens = scanner.scan_tokens()
         for token in tokens:
             print(token)
-
-    def error(self, line: int, column: int, message: str) -> None:
-        """Gathering error information for reporting.
-
-        Added column information as suggested on page 42.
-        """
-        self.report(line, column, "", message)
-
-    def report(self, line: int, column: int, where: str, message: str) -> None:
-        """Reporting error"""
-        if where:
-            where = "in " + where
-        sys.stderr.write(" " * (column + len(self.prompt)) + "^-- Error.\n")
-        sys.stderr.write(f"[line {line}] {where}: {message}\n")
-        self.had_error = True
